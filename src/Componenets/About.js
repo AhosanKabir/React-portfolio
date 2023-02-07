c51e711be5264d0285874f66fa1cf800c2072bcb
ahosan-kabir <kaziahosan1998@gmail.com>
2023-02-07 21:59:42 +0600

    add

--- src/Componenets/About.js
+++ src/Componenets/About.js
@@ -26,7 +26,8 @@ const About = () => {
             {/* <img src="/img/pic.png" alt="my-pictures" className="img-fluid" /> */}
             <div class="about-image">
               <img
-                src={require("../assets/pic.png")}
+                // src={require("../assets/pic.png")}
+                src="/img/pic.png"
                 alt="my-pictures"
                 className="img-fluid owner_img"
               />
