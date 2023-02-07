5ddc0a800d902f5fb9b2a7db368a6ca0555e8569
ahosan-kabir <kaziahosan1998@gmail.com>
2023-02-07 21:24:12 +0600

    added-pic

--- src/Componenets/About.js
+++ src/Componenets/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+// import myPic from "../../public/img/pic.png"
 
 const About = () => {
   return (
@@ -25,7 +26,7 @@ const About = () => {
             {/* <img src="/img/pic.png" alt="my-pictures" className="img-fluid" /> */}
             <div class="about-image">
               <img
-                src="/img/pic.png"
+                src={require("../assets/pic.png")}
                 alt="my-pictures"
                 className="img-fluid owner_img"
               />
